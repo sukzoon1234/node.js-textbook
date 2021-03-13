31da5829613277ce4e4b4be1f3e3935c1cbb60a7
sukzoon**** <sukzoon1234@naver.com>
2021-03-13 10:29:48 +0000

    2021.03.13

--- ch4/4.2/restServer.js
+++ ch4/4.2/restServer.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const fs = require('fs').promises;
 
+const users = {}; // 데이터 저장용
+
 http.createServer(async (req, res) => {
 	try {
 		console.log(req.method, req.url);
-		if(req.method === 'GET') {
-			if(req.url === '/') {
+		if (req.method === 'GET') {
+			if (req.url === '/') {
 				const data = await fs.readFile('./restFront.html');
 				res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 				return res.end(data);
@@ -13,15 +15,24 @@ http.createServer(async (req, res) => {
 				const data = await fs.readFile('./about.html');
 				res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 				return res.end(data);
+			} else if (req.url === '/users') {
+				res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+				return res.end(JSON.stringify(users));
 			}
-			//주소가 /도 /about도 아니라면??
+			//주소가 /도 /about도 /users도 아니라면??
 			try {
-				const data = await fs.readFile(`${req.url}`);
+				const data = await fs.readFile(`./${req.url}`);
 				return res.end(data);
 			} catch (err) {
 				//주소에 해당하는 라우트를 못 찾았다는 404 not found error 발생
 			}
-		} 
+		} else if (req.method === 'POST') {
+			
+		} else if (req.method === 'PUT') {
+			
+		} else if (req.method === 'DELETE') {
+			
+		}
 		res.writeHead(404);
 		return res.end('NOT FOUND');
 	} catch (err) {
